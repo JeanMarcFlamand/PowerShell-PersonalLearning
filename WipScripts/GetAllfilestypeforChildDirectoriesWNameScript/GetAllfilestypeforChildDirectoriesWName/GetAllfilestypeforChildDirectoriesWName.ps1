b801bc19b5d16c8a8f6f997f6478a7a27523b7f8
Jean-Marc Flamand <7jtf@videotron.ca>
2023-09-04 07:06:18 -0400

    add Script project

---
+++ WipScripts/GetAllfilestypeforChildDirectoriesWNameScript/GetAllfilestypeforChildDirectoriesWName/GetAllfilestypeforChildDirectoriesWName.ps1
@@ -0,0 +1,19 @@
+﻿# Specify the drive on which you want to perform the search
+$drive = "D:\"
+
+# Search for child directories with the name "wip"
+$wipDirectories = Get-ChildItem -Path $drive -Recurse | Where-Object { $_.PSIsContainer -and $_.Name -eq "wip" -and $_.FullName -notlike 'C:\Windows*' }
+# Traverse through the "wip" directories and check for XLS files
+foreach ($directory in $wipDirectories) {
+    $directoryPath = $directory.FullName
+    $xlsFiles = Get-ChildItem -Path $directoryPath -Filter "*.xls" -File
+
+    if ($xlsFiles.Count -gt 0) {
+        Write-Host "Directory '$directoryPath' contains XLS files:"
+        $xlsFiles | ForEach-Object {
+            Write-Host " - $($_.Name)"
+        }
+    } else {
+        Write-Host "Directory '$directoryPath' does not contain any XLS files."
+    }
+}
