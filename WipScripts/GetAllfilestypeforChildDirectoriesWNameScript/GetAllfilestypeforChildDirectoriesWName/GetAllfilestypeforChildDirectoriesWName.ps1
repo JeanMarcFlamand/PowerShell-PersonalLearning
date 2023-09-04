24dd9d73980d9492812d82653871154a7d844704
Jean-Marc Flamand <7jtf@videotron.ca>
2023-09-04 07:26:03 -0400

    Update to handle the case where $wipDirectories may be empty or null (i.e., no "wip" directories were found that meet the specified criteria)

--- WipScripts/GetAllfilestypeforChildDirectoriesWNameScript/GetAllfilestypeforChildDirectoriesWName/GetAllfilestypeforChildDirectoriesWName.ps1
+++ WipScripts/GetAllfilestypeforChildDirectoriesWNameScript/GetAllfilestypeforChildDirectoriesWName/GetAllfilestypeforChildDirectoriesWName.ps1
@@ -3,17 +3,25 @@ $drive = "D:\"
 
 # Search for child directories with the name "wip"
 $wipDirectories = Get-ChildItem -Path $drive -Recurse | Where-Object { $_.PSIsContainer -and $_.Name -eq "wip" -and $_.FullName -notlike 'C:\Windows*' }
-# Traverse through the "wip" directories and check for XLS files
-foreach ($directory in $wipDirectories) {
-    $directoryPath = $directory.FullName
-    $xlsFiles = Get-ChildItem -Path $directoryPath -Filter "*.xls" -File
+# Check if $wipDirectories is empty or null
+if ($wipDirectories -eq $null -or $wipDirectories.Count -eq 0)
+{
+    Write-Host "No 'wip' directories found that meet the specified criteria."
+}
+else
+{
+    # Traverse through the "wip" directories and check for XLS files
+    foreach ($directory in $wipDirectories) {
+        $directoryPath = $directory.FullName
+        $xlsFiles = Get-ChildItem -Path $directoryPath -Filter "*.xls" -File
 
-    if ($xlsFiles.Count -gt 0) {
-        Write-Host "Directory '$directoryPath' contains XLS files:"
-        $xlsFiles | ForEach-Object {
-            Write-Host " - $($_.Name)"
+        if ($xlsFiles.Count -gt 0) {
+            Write-Host "Directory '$directoryPath' contains XLS files:"
+            $xlsFiles | ForEach-Object {
+                Write-Host " - $($_.Name)"
+            }
+        } else {
+            Write-Host "Directory '$directoryPath' does not contain any XLS files."
         }
-    } else {
-        Write-Host "Directory '$directoryPath' does not contain any XLS files."
     }
 }
